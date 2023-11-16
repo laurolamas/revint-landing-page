30cad6bb4656dd2f1c1ff870c7f2c2922999fe6e
Nacho228 <6319@holbertonstudents.com>
2023-11-16 11:39:17 -0800

    colors

--- app/page.js
+++ app/page.js
@@ -59,22 +59,22 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="h-1/2 flex-row " style={{ backgroundImage: 'url(/jean.png)' }}>
+        <div className="h-2/6 flex-row bg-marron/50">
           <div className=" mx-auto md:px-6">
-            <section className="mb-32 text-center">
-              <h2 className="mb-20 text-3xl font-bold">Features</h2>
+            <section className=" text-center">
+              <h2 className="mb-20 text-5xl font-serif font-semibol text-brown">Features</h2>
               <div className="grid lg:grid-cols-3 lg:gap-x-12">
-                <div className="mb-12 lg:mb-0">
+                <div className=" lg:mb-0">
                   <div
-                    className="block h-full rounded-xl bg-black/75">
+                    className="block h-full rounded-xl bg-fack">
                     <div className="flex justify-center">
-                      <div className="-mt-8 inline-block rounded-full bg-white p-4 text-primary shadow-md">
+                      <div className="-mt-8 inline-block rounded-full bg-brown p-4 text-primary shadow-md">
                         <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" className="h-8 w-8"><path fill="#000000" d="M704 288h131.072a32 32 0 0 1 31.808 28.8L886.4 512h-64.384l-16-160H704v96a32 32 0 1 1-64 0v-96H384v96a32 32 0 0 1-64 0v-96H217.92l-51.2 512H512v64H131.328a32 32 0 0 1-31.808-35.2l57.6-576a32 32 0 0 1 31.808-28.8H320v-22.336C320 154.688 405.504 64 512 64s192 90.688 192 201.664v22.4zm-64 0v-22.336C640 189.248 582.272 128 512 128c-70.272 0-128 61.248-128 137.664v22.4h256zm201.408 483.84L768 698.496V928a32 32 0 1 1-64 0V698.496l-73.344 73.344a32 32 0 1 1-45.248-45.248l128-128a32 32 0 0 1 45.248 0l128 128a32 32 0 1 1-45.248 45.248z" />
                         </svg>
                       </div>
                     </div>
                     <div className="p-6">
-                      <h5 className="mb-4 text-lg font-semibold">Create your own products</h5>
+                      <h5 className="mb-4 text-lg font-semibold text-black">Create your own products</h5>
                       <p>
                         Create your products and publish them never been so easy, follow certain parameters and look for potential costumers.
                       </p>
@@ -83,14 +83,14 @@ export default function Home() {
                 </div>
                 <div className="mb-12 lg:mb-0">
                   <div
-                    className="block h-full rounded-xl bg-black/75">
-                    <div className="flex justify-center">
-                      <div className="-mt-8 inline-block rounded-full p-4 text-primary bg-white shadow-md">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="" viewBox="" strokeWidth="1"
-                          stroke="currentColor" className="h-7 w-8">
-                          <path d="M21.886 5.115c3.521 0 6.376 2.855 6.376 6.376 0 1.809-0.754 3.439-1.964 4.6l-10.297 10.349-10.484-10.536c-1.1-1.146-1.778-2.699-1.778-4.413 0-3.522 2.855-6.376 6.376-6.376 2.652 0 4.925 1.62 5.886 3.924 0.961-2.304 3.234-3.924 5.886-3.924zM21.886 4.049c-2.345 0-4.499 1.089-5.886 2.884-1.386-1.795-3.54-2.884-5.886-2.884-4.104 0-7.442 3.339-7.442 7.442 0 1.928 0.737 3.758 2.075 5.152l11.253 11.309 11.053-11.108c1.46-1.402 2.275-3.308 2.275-5.352 0-4.104-3.339-7.442-7.442-7.442v0z" fill="#000000"></path>
+                    className="block h-full rounded-xl bg-fack">
+                    <div className="flex justify-center ">
+                      <div className="-mt-8 rounded-full p-4 shadow-md bg-brown">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="" color="black" stroke="currentColor" strokeWidth="1" className="h-7 w-8">
+                          <path fill="#00000" d="M21.886 5.115c3.521 0 6.376 2.855 6.376 6.376 0 1.809-0.754 3.439-1.964 4.6l-10.297 10.349-10.484-10.536c-1.1-1.146-1.778-2.699-1.778-4.413 0-3.522 2.855-6.376 6.376-6.376 2.652 0 4.925 1.62 5.886 3.924 0.961-2.304 3.234-3.924 5.886-3.924zM21.886 4.049c-2.345 0-4.499 1.089-5.886 2.884-1.386-1.795-3.54-2.884-5.886-2.884-4.104 0-7.442 3.339-7.442 7.442 0 1.928 0.737 3.758 2.075 5.152l11.253 11.309 11.053-11.108c1.46-1.402 2.275-3.308 2.275-5.352 0-4.104-3.339-7.442-7.442-7.442v0z"></path>
                         </svg>
                       </div>
+
                     </div>
                     <div className="p-6">
                       <h5 className="mb-4 text-lg font-semibold">Like products that you like</h5>
@@ -103,9 +103,9 @@ export default function Home() {
 
                 <div className="">
                   <div
-                    className="block h-full rounded-xl bg-black/75">
+                    className="block h-full rounded-xl bg-fack">
                     <div className="flex justify-center">
-                      <div className="-mt-8 inline-block rounded-full bg-white  p-4 text-primary shadow-md">
+                      <div className="-mt-8 inline-block rounded-full bg-brown  p-4 text-primary shadow-md">
                         <svg viewBox="0 -8 72 72" id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" className="h-9 w-9"><path d="M64,12.78v17s-3.63.71-4.38.81-3.08.85-4.78-.78C52.22,27.25,42.93,18,42.93,18a3.54,3.54,0,0,0-4.18-.21c-2.36,1.24-5.87,3.07-7.33,3.78a3.37,3.37,0,0,1-5.06-2.64,3.44,3.44,0,0,1,2.1-3c3.33-2,10.36-6,13.29-7.52,1.78-1,3.06-1,5.51,1C50.27,12,53,14.27,53,14.27a2.75,2.75,0,0,0,2.26.43C58.63,14,64,12.78,64,12.78ZM27,41.5a3,3,0,0,0-3.55-4.09,3.07,3.07,0,0,0-.64-3,3.13,3.13,0,0,0-3-.75,3.07,3.07,0,0,0-.65-3,3.38,3.38,0,0,0-4.72.13c-1.38,1.32-2.27,3.72-1,5.14s2.64.55,3.72.3c-.3,1.07-1.2,2.07-.09,3.47s2.64.55,3.72.3c-.3,1.07-1.16,2.16-.1,3.46s2.84.61,4,.25c-.45,1.15-1.41,2.39-.18,3.79s4.08.75,5.47-.58a3.32,3.32,0,0,0,.3-4.68A3.18,3.18,0,0,0,27,41.5Zm25.35-8.82L41.62,22a3.53,3.53,0,0,0-3.77-.68c-1.5.66-3.43,1.56-4.89,2.24a8.15,8.15,0,0,1-3.29,1.1,5.59,5.59,0,0,1-3-10.34C29,12.73,34.09,10,34.09,10a6.46,6.46,0,0,0-5-2C25.67,8,18.51,12.7,18.51,12.7a5.61,5.61,0,0,1-4.93.13L8,10.89v19.4s1.59.46,3,1a6.33,6.33,0,0,1,1.56-2.47,6.17,6.17,0,0,1,8.48-.06,5.4,5.4,0,0,1,1.34,2.37,5.49,5.49,0,0,1,2.29,1.4A5.4,5.4,0,0,1,26,34.94a5.47,5.47,0,0,1,3.71,4,5.38,5.38,0,0,1,2.39,1.43,5.65,5.65,0,0,1,1.48,4.89,0,0,0,0,1,0,0s.8.9,1.29,1.39a2.46,2.46,0,0,0,3.48-3.48s2,2.48,4.28,1c2-1.4,1.69-3.06.74-4a3.19,3.19,0,0,0,4.77.13,2.45,2.45,0,0,0,.13-3.3s1.33,1.81,4,.12c1.89-1.6,1-3.43,0-4.39Z" /></svg>
                       </div>
                     </div>
@@ -121,7 +121,6 @@ export default function Home() {
             </section>
           </div>
         </div>
-
         <div className="h-full bg-cover flex-items-center justify-center" style={{ backgroundImage: 'url(/recycle.jpg)' }}>
           <div className="h-full w-full bg-black/50">
             <div className="relative">
@@ -139,63 +138,59 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="h-full bg-cover bg-center bg-black opacity-80" style={{ backgroundImage: 'url(/lana2.jpg)' }}>
-          <Typography variant="h1" color="white" className="flex items-center justify-center">
+        <div className="h-full bg-cover bg-center opacity-80 mb-20 bg-fack">
+          <div className='bg-black h-auto w-full flex-col'>
+            <div className="h-1/2 items-center mb-12 ">
+              <Typography variant="h1" color="white" className=" flex items-center text-2xl justify-start mb-12">
+                About us
+              </Typography>
+              <Typography variant="lead" className="justify-start text-xl w-1/3">We are software development students who been motivated by the non-existent or non-functional online vintage stores in Uruguay. Our mindset is letting users to feel free by selling their products, the goal is connect those users with people who would want to buy them.</Typography>
+            </div>
+          </div>
+          <h1 className="text-3xl text-semibold border-t-4 border-beige flex items-center justify-center">
             Our team
-          </Typography>
-          <div className="w-full justify-evenly flex mt-20">
-            <div className="card w-96 bg-transparent shadow-xl">
+          </h1>
+          <div className="w-full justify-evenly flex mb-32 bg-beige">
+            <div className=" w-96 ">
               <figure className="px-10 pt-10">
-                <img src="./d.jpg" alt="foto" className="rounded-xl" />
+                <img src="./igni.jpg" alt="foto" className="rounded-xl" />
               </figure>
-              <div className="card-body items-center text-center">
-                <Typography variant="h3" color="black" className="flex items-center justify-center">
+              <div className=" card-body items-center text-center ">
+                <h3 className="text-marron flex text-xl items-center justify-center">
                   Ignacio Llanes / Front-End Developer
-                </Typography>
-                <Image src={NachoSVG} alt="Nacho QR" width={200} height={200} />
+                </h3>
               </div>
             </div>
-            <div className="card w-96 bg-transparent shadow-xl">
+            <div className="w-96">
               <figure className="px-10 pt-10">
                 <img src="./a.jpeg" alt="foto" className="rounded-xl" />
               </figure>
               <div className="card-body items-center text-center">
-                <Typography variant="h3" color="white" className="flex items-center justify-center">
+              <h3 className="text-marron flex text-xl items-center justify-center">
                   Facundo Machado Back-End Developer / Project Manager
-                </Typography>
-                <Image src={KundiSVG} alt="Facundo SVG" width={200} height={200} />
+                </h3>
               </div>
             </div>
-            <div className="card w-96 bg-transparent shadow-xl">
+            <div className="w-96">
               <figure className="px-10 pt-10">
-                <img src="./b.jpeg" alt="foto" className="rounded-xl" />
+                <img src="./ale.jpeg" alt="foto" className="rounded-xl" />
               </figure>
               <div className="card-body items-center text-center" style={images}>
-                <Typography variant="h3" color="white" className="flex items-center justify-center">
+              <h3 className="text-marron flex text-xl items-center justify-center">
                   Alejandro Rivello / Front-End Developer
-                </Typography>
-                <Image src={AleSVG} alt="Ale SVG" width={200} height={200} />
+                </h3>
               </div>
             </div>
-            <div className="card w-96 bg-transparent shadow-xl">
+            <div className="w-96">
               <figure className="px-10 pt-10">
                 <img src="./c.jpeg" alt="foto" className="rounded-xl" />
               </figure>
               <div className="card-body items-center text-center">
-                <Typography variant="h3" color="white" className="flex items-center justify-center">
+              <h3 className="text-marron flex text-xl items-center justify-center">
                   Lauro Lamas / Full-Stack Developer
-                </Typography>
-                <Image src={LauroSVG} alt="Lauro QR" width={200} height={200} />
+                </h3>
               </div>
             </div>
-          </div>
-        </div>
-        <div className='bg-black h-1/2 w-full flex-col mt-16'>
-          <div className="h-1/2 items-center mt-32">
-            <Typography variant="h1" color="white" className="flex items-center justify-center mb-0">
-              About us
-            </Typography>
-            <Typography variant="h3" className="items-center justify-center mt-16 text-center">We are software development students who been motivated by the non-existent or non-functional online vintage stores in Uruguay. Our mindset is letting users to feel free by selling their products, the goal is connect those users with people who would want to buy them.</Typography>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors:{
+      'beige': '#F5F5DC',
+      'brown': '#f2e8e5',
+      'fack': '#846358',
+      'gray': '#777B7E',
+      'marron': '#43302b',
+      'columbia': '#D1EAF0',
+      'prussian': '#012A4A',
+      'asdd': '#00000',
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
